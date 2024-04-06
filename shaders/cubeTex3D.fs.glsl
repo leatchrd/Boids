8b305ec437f909827163a74fc7cc1bee91832c1e
Sara Lafleur <sara.m.lafleur@gmail.com>
2024-04-06 11:26:28 +0200

    Fix wall color blending

--- shaders/cubeTex3D.fs.glsl
+++ shaders/cubeTex3D.fs.glsl
@@ -4,10 +4,10 @@ in vec3 vPosition_vs;  // in View Space
 in vec3 vNormal_vs;    // in View Space
 in vec2 vTexCoords;
 
-out vec3 fFragTexture;
+out vec4 fFragTexture;
 
 uniform sampler2D uTextureGlass;
 
 void main() {
-  fFragTexture = vec3(texture(uTextureGlass, vTexCoords)).xyz;
+  fFragTexture = vec4((texture(uTextureGlass, vTexCoords)).xyz, 0.25);
 }
